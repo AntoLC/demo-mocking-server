82c43141a7d263406786f24fe66ea278b95e71f7
ALC Consulting <freelance.anthony@gmail.com>
2022-09-26 16:34:39 +0200

    list_from_api

---
+++ src/conf.ts
@@ -0,0 +1,7 @@
+export const optionsRapidAPI = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "363a91db89msh26e6c3267eae296p1d867bjsn8fb6f21af18b",
+    "X-RapidAPI-Host": "imdb8.p.rapidapi.com",
+  },
+};
