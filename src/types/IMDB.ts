668e23c43db3f1243a2150baf20be7f0d4d33eaf
ALC Consulting <freelance.anthony@gmail.com>
2022-09-26 16:34:39 +0200

    list_from_api

---
+++ src/types/IMDB.ts
@@ -0,0 +1,23 @@
+export interface IIMDB {
+  d: IDataIMDB[];
+  q: string;
+  v: number;
+}
+
+export interface IDataIMDB {
+  i: IImageIMDB;
+  id: string;
+  l: string;
+  s: string;
+  q?: string;
+  qid?: string;
+  rank?: number;
+  y?: number;
+  yr?: string;
+}
+
+export interface IImageIMDB {
+  height: number;
+  imageUrl: string;
+  width: number;
+}
